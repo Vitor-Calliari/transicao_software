e7df5d4add2e3e0ce4774dc5803bae262d6ef48d
izaweigert <izadora.weigert@alunos.bpkedu.com.br>
2025-11-18 20:30:54 -0300

    chore: postgres.db

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
